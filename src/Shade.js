176333d68ceb24e4709ad56cdd234cdfe5ae26ea
debbie chong <giyoungchong1980@gmail.com>
2018-04-14 14:23:32 -0400

    Test state

---
+++ src/Shade.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Shade extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: 'sunny'
+    };
+    this.toggleShade = this.toggleShade.bind(this);
+  }
+
+  toggleShade(){
+    const newShade = this.state.weather == 'sunny' ?
+    'cloudy' : 'sunny';
+    this.setState({weather: newShade });
+  }
+
+  render(){
+    return (
+      <div>
+       <h3> it is so {this.state.weather}!</h3>
+       <button onClick={this.toggleShade}>
+       mother nature fluxxxx
+       </button>
+      </div>
+    );
+  }
+}
+
+export default Shade;
